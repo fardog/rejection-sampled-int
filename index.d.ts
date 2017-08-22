333281b555a6c5b9042b5b22278d9176043bc889
Nathan Wittstock <nate@milkandtang.com>
2017-08-21 23:31:29 -0700

    add typescript definitions (#2)

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+// Type definitions for rejection-sampled-int 0.1.0
+// Project: rejection-sampled-int
+// Definitions by: Nathan Wittstock <fardog.io>
+
+export as namespace randomRejectionSampledIntLib
+
+export = randomRejectionSampledInt
+
+declare function randomRejectionSampledInt(): number
+declare function randomRejectionSampledInt(min: number, max?: number): number
+
+declare namespace randomRejectionSampledInt {}
